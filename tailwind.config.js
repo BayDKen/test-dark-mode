11123c145963f77674c3da75a8ae97bbf6b53831
BayDKen <144319861+BayDKen@users.noreply.github.com>
2023-09-26 14:45:37 +0700

    add: component_modal_image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ['./**/*.{html,js}', './node_modules/flowbite/**/*.js'],
   theme: {
+    fontFamily: {
+      'RedHat': ["Red Hat Display, sans-serif"],
+    },
     extend: {},
   },
   plugins: [
